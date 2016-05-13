768a375e4a13c544140cca26b3cd478f46ceb34a
Jorge Sanchez <jorjatorz@gmail.com>
2016-05-13 17:25:42 +0200

    Implementd ACota_Ingenua_Optimista

---
+++ PracticaMARP/ACota_Ingenua_Optimista.cpp
@@ -0,0 +1,51 @@
+#include "ACota_Ingenua_Optimista.h"
+
+#include "Mapa.h"
+
+ACota_Ingenua_Optimista::ACota_Ingenua_Optimista(const Mapa& map)
+{
+	_distanciasMinimas = new int[map.getNumeroDeCiudades()];
+	calcularDistanciasMinimas(map);
+}
+
+
+ACota_Ingenua_Optimista::~ACota_Ingenua_Optimista()
+{
+	delete [] _distanciasMinimas;
+}
+
+int ACota_Ingenua_Optimista::calcularCoste(int distanciaActual, int numCiudades, bool ciudadUsada[]) const
+{
+	int suma = distanciaActual;
+	for (int i = 0; i < numCiudades; i++)
+	{
+		if (!ciudadUsada[i])
+		{
+			suma += _distanciasMinimas[i];
+		}
+	}
+
+	return suma;
+}
+
+void ACota_Ingenua_Optimista::calcularDistanciasMinimas(const Mapa& map)
+{
+	for (int i = 0; i < map.getNumeroDeCiudades(); i++)
+	{
+		int min = INT_MAX;
+		for (int j = 0; j < map.getNumeroDeCiudades(); j++)
+		{
+			if (j != i)
+			{
+				int dist = map.getDistanciaEntreCiudades(i, j);
+
+				if (dist < min)
+				{
+					min = dist;
+				}
+			}
+		}
+
+		_distanciasMinimas[i] = min;
+	}
+}
---
+++ PracticaMARP/ACota_Ingenua_Optimista.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ACota.h"
+
+class Mapa;
+
+class ACota_Ingenua_Optimista : public ACota
+{
+public:
+	ACota_Ingenua_Optimista(const Mapa& map);
+	~ACota_Ingenua_Optimista();
+
+	//Suma la distancia minima de las ciudades aun sin elegir.
+	//Esta distancia minima ha sido calculado anteriormente
+	//O(n)
+	virtual int calcularCoste(int distanciaActual, int numCiudades, bool ciudadUsada[]) const;
+
+private:
+	//Dado un mapa, calcula la distancia menor de una ciudad a las demas.
+	//Es decir, mira todas las distancia desde Madrid a las demas ciudades y guarda la minima
+	//O(n^2)
+	void calcularDistanciasMinimas(const Mapa& map);
+	int *_distanciasMinimas;
+
+};
+
